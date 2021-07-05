2d0aeb6016dcb743e7fb8a7849fd5f64b4ac6009
Sandra Munoz <sandra.m@michelada.io>
2021-07-05 10:58:55 -0500

    Add Rest class to divide resources by modules and endpoints
    I added two files: rest.rb and /rest/people.rb to split the modules and
    logic of the endponts. I had issues trying to load the files, that's why
    the modules are under the main module of ApiSalesloft

--- lib/api_salesloft.rb
+++ lib/api_salesloft.rb
@@ -9,11 +9,14 @@ require 'api_salesloft/client'
 require 'pry'
 
 module ApiSalesloft
-  class Error < StandardError; end
+  class Error < StandardError;
+  end
 
-  class InvalidRequestError < StandardError; end
+  class InvalidRequestError < StandardError;
+  end
 
-  class InvalidResponseError < StandardError; end
+  class InvalidResponseError < StandardError;
+  end
 
   def self.configuration
     @configuration ||= Configuration.new
@@ -23,7 +26,22 @@ module ApiSalesloft
     yield(configuration)
   end
 
-  def self.client(token)
-    Client.new(auth_token: token)
+  def self.rest(token)
+    Rest.new(Client.new(auth_token: token || configuration.auth_token))
+  end
+
+  #Add Rest class to call it from the ApiSalesloft. This method class allow the client instance to fetch the people list from controller
+  class Rest
+    def initialize(client)
+      @client = client
+    end
+
+    def list_people
+      client.get(path: '/people.json')
+    end
+
+    private
+
+    attr_reader :client
   end
 end
